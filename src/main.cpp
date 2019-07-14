75d18bd9fd1066848379c67eebead1101c9c63ca
nullpoint-dev <jfetizanan2191@gmail.com>
2019-07-15 00:36:55 +0800

    All portal resources converted to base64 and embeded.

--- src/main.cpp
+++ src/main.cpp
@@ -10,33 +10,19 @@ const byte DNS_PORT = 53;
 IPAddress apIP(172, 0, 0, 1);
 DNSServer dnsServer;
 
-class CaptiveRequestHandler : public AsyncWebHandler {
+class CaptiveRequestHandler : public AsyncWebHandler
+{
 public:
   CaptiveRequestHandler() {}
   virtual ~CaptiveRequestHandler() {}
 
-  bool canHandle(AsyncWebServerRequest *request){
+  bool canHandle(AsyncWebServerRequest *request)
+  {
     return true;
   }
-  void handleRequest(AsyncWebServerRequest *request) {
-    if(request->url() == "/rick.ogg") {
-      request->send(SPIFFS, "/rick.ogg", "audio/ogg");
-    }
-    else if(request->url() == "/giphy.webp") {
-       request->send(SPIFFS, "/giphy.webp", "image/webp");
-    }
-    else if(request->url() == "/bootstrap.min.css") {
-       request->send(SPIFFS, "/bootstrap.min.css", "text/css");
-    }
-    else if(request->url() == "/scrolling-nav.css") {
-       request->send(SPIFFS, "/scrolling-nav.css", "text/css");
-    }
-    else if(request->url() == "/logo-ironwifi.png") {
-       request->send(SPIFFS, "/logo-ironwifi.png", "image/png");
-    }
-    else {
-      request->send(SPIFFS, "/index.html", "text/html");
-    }
+  void handleRequest(AsyncWebServerRequest *request)
+  {
+    request->send(SPIFFS, "/index.html", "text/html");
   }
 };
 
